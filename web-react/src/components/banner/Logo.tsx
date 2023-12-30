50eb08c88bfb2b0714d5e757f452f89885fa7107
Luis Miguel Urbez <lmurbezv@gmail.com>
2023-12-30 12:37:32 +0100

    Imaganes en el banner V2

--- web-react/src/components/banner/Logo.tsx
+++ web-react/src/components/banner/Logo.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+
 
 
 const Logo = () => {
-  const router = useRouter()
   return (
     <div className="relative">
         <div onClick={() =>{window.open('https://github.com/LuisMi01/airbnb-mongodb_LuisMiguelUrbez', '_blank');}
