3e28ece52b6a8902dc57b4bd2807025c7fbae428
Luis Miguel Urbez <lmurbezv@gmail.com>
2023-12-30 13:36:18 +0100

    Incluido el componente Index.js

---
+++ web-react/src/Index.js
@@ -0,0 +1,9 @@
+import Layout from 'dynamic-material-ui/docs/data/layout'
+import * as root from 'react-dom'
+import React from 'react'
+
+root.render(
+  <React.StrictMode>
+    <Layout><div>Contenido</div></Layout>
+  </React.StrictMode>
+);
