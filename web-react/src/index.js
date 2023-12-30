e520688129ad5c9961f6a938cb40f721c318a3ad
Luis Miguel Urbez <lmurbezv@gmail.com>
2023-12-30 16:15:28 +0100

    sin funcionalidad page.tsx

--- web-react/src/index.js
+++ web-react/src/index.js
@@ -1,8 +1,18 @@
+
+import './index.css';
+import Layout from '../src/Layout';
+import reportWebVitals from './reportWebVitals';
 import * as root from 'react-dom'
 import React from 'react'
+
 
 root.render(
   <React.StrictMode>
     <Layout><div>Contenido</div></Layout>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
