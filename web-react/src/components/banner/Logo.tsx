39d584b8448f0a0ad9a19d537cd931b2acdf0b09
Luis Miguel Urbez <lmurbezv@gmail.com>
2023-12-30 12:35:14 +0100

    Imaganes en el banner

--- web-react/src/components/banner/Logo.tsx
+++ web-react/src/components/banner/Logo.tsx
@@ -7,7 +7,16 @@ import { useRouter } from 'next/navigation'
 const Logo = () => {
   const router = useRouter()
   return (
-    <Image onClick={() => router.push('/')} alt="Logo" className="ms:block cursor-pointer" src="/images/logo.png" width="100" height="100"/>
+    <div className="relative">
+        <div onClick={() =>{window.open('https://github.com/LuisMi01/airbnb-mongodb_LuisMiguelUrbez', '_blank');}
+        } className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+          <Image src='/images/githublogo.png' alt={"Github"} width={30} height={30}/>
+        </div>
+        <div onClick={() =>{window.open('https://uax.com', '_blank');}
+        } className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+          <Image src='/images/uaxlogo.png' alt={"Uax"} width={30} height={30}/>
+        </div>
+    </div>
   )
 }
 
--- web-react/src/components/banner/Search.tsx
+++ web-react/src/components/banner/Search.tsx
@@ -1,28 +1,14 @@
 'use client'
 import { BiSearch } from 'react-icons/bi';
-import {useSearchParams} from "next/navigation";
-import {useMemo} from "react";
-import {differenceInDays} from "date-fns";
 
 const Search = () => {
-
   return (
-    <div  className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+    <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
       <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6">
-
-        </div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-
-        </div>
-        <div className=" text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">
-
-          </div>
-          <div className="p-2 bg-rose-500 rounded-full text-white">
-            <BiSearch size={20} />
-          </div>
-        </div>
+        <input type="text" className="text-sm font-semibold px-6" placeholder="Buscar..."/>
+        <button className="p-2 bg-rose-500 rounded-full text-white">
+          <BiSearch size={20} />
+        </button>
       </div>
     </div>
   );
